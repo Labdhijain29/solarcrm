b9dfca0254dbfafe4353f6793e40cf996a24b522
Abpa007 <abhaypanchal095@gmail.com>
2026-04-27 19:06:53 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -73,6 +73,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 SolarCRM Backend running on http://localhost:${PORT}`);
   console.log(`📄 Environment: ${process.env.NODE_ENV}`);
+  console.log(`📄 NEWWWWWWW`);
 });
 
 module.exports = app;
